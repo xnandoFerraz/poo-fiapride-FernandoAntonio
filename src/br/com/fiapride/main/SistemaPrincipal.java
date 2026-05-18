f638b39b7a1df086844c1bbf08440a1c905621f3
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-18 09:29:43 -0300

    feat: aula2 adiciona metodos com parametros e retorno

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -1,30 +1,98 @@
 package br.com.fiapride.main;
 
-// Importamos a classe Cachorro para que o sistema a reconheça
 import br.com.fiapride.model.Cachorro;
 
 public class SistemaPrincipal {
 
     public static void main(String[] args) {
+
         // INSTANCIAÇÃO
-        // O comando 'new' aloca memória para um novo objeto.
-        // Criando o primeiro passageiro (Objeto 1)
-    	Cachorro cachorro1 = new Cachorro();
+        Cachorro cachorro1 = new Cachorro();
+
         cachorro1.cor = "Cinza";
-        cachorro1.peso = 20; //kg
-        cachorro1.tamanho = 55; //cm
+        cachorro1.peso = 20;
+        cachorro1.tamanho = 55;
+        cachorro1.energia = 10;
         
-
-        // Criando o segundo passageiro (Objeto 2)
         Cachorro cachorro2 = new Cachorro();
-        cachorro2.cor = "Marrom";
-        cachorro2.peso = 35; //kg
-        cachorro2.tamanho = 60; //cm
 
-        // Exibindo os dados no Console
+        cachorro2.cor = "Marrom";
+        cachorro2.peso = 35;
+        cachorro2.tamanho = 60;
+        cachorro2.energia = 10;
+
+
+        // EXIBIÇÃO INICIAL
         System.out.println("--- Sistema FiapRide ---");
-        System.out.println("Meu cachorro é: " + cachorro1.cor);
-        System.out.println("O cachorro do professor é: " + cachorro2.cor);
         
+        System.out.println();
+
+        System.out.println("--Cachorro 1--");
+        System.out.println("Cor: " + cachorro1.cor);
+        System.out.println("Peso: " + cachorro1.peso + "kg");
+        System.out.println("Tamanho inicial: " + cachorro1.tamanho + "cm");
+        System.out.println("Energia inicial: " + cachorro1.energia);
+        
+        System.out.println();
+        
+        System.out.println("--Cachorro 2--");
+        System.out.println("Cor: " + cachorro2.cor);
+        System.out.println("Peso: " + cachorro2.peso + "kg");
+        System.out.println("Tamanho inicial: " + cachorro2.tamanho + "cm");
+        System.out.println("Energia inicial: " + cachorro2.energia);
+
+        System.out.println();
+
+        // TESTE COM VALORES VÁLIDOS
+        System.out.println("-- Testes válidos --");
+
+        cachorro1.crescer(5);
+        cachorro1.brincar(4);
+
+        System.out.println();
+
+        // ESTADO APÓS ALTERAÇÕES
+        System.out.println("--- Estado atualizado ---");
+        
+        System.out.println("--Cachorro 1--");
+        System.out.println("Cor: " + cachorro1.cor);
+        System.out.println("Peso: " + cachorro1.peso + "kg");
+        System.out.println("Tamanho atualizado: " + cachorro1.tamanho + "cm");
+        System.out.println("Energia atualizado: " + cachorro1.energia);
+        
+        System.out.println();
+        
+        System.out.println("--Cachorro 2--");
+        System.out.println("Cor: " + cachorro2.cor);
+        System.out.println("Peso: " + cachorro2.peso + "kg");
+        System.out.println("Tamanho atualizado: " + cachorro2.tamanho + "cm");
+        System.out.println("Energia atualizado: " + cachorro2.energia);
+
+        System.out.println();
+
+        // TESTE COM VALORES INVÁLIDOS
+        System.out.println("-- Testes inválidos --");
+
+        cachorro1.crescer(0);
+        cachorro1.brincar(20);
+        
+        System.out.println();
+        
+        System.out.println("--- Estado atualizado ---");
+        
+        System.out.println("--Cachorro 1--");
+        System.out.println("Cor: " + cachorro1.cor);
+        System.out.println("Peso: " + cachorro1.peso + "kg");
+        System.out.println("Tamanho atualizado: " + cachorro1.tamanho + "cm");
+        System.out.println("Energia atualizado: " + cachorro1.energia);
+        
+        System.out.println();
+        
+        System.out.println("--Cachorro 2--");
+        System.out.println("Cor: " + cachorro2.cor);
+        System.out.println("Peso: " + cachorro2.peso + "kg");
+        System.out.println("Tamanho atualizado: " + cachorro2.tamanho + "cm");
+        System.out.println("Energia atualizado: " + cachorro2.energia);
+
     }
 }
--- src/br/com/fiapride/model/Cachorro.java
+++ src/br/com/fiapride/model/Cachorro.java
@@ -1,9 +1,35 @@
 package br.com.fiapride.model;
-public class Cachorro { // <-- Use o nome do SEU objeto    
-    // As características que você pensou    
+public class Cachorro {
+
+    // ATRIBUTOS
     public String cor;
-    public float peso;
-    public float tamanho;
-    
-    
+    public int peso;
+    public int tamanho;
+    public int energia;
+
+    // Método 1
+    // O cachorro só pode crescer se o valor for positivo
+    public void crescer(int cm) {
+        if (cm > 0) {
+            tamanho = tamanho + cm;
+            System.out.println("O cachorro cresceu " + cm + "cm.");
+        } else {
+            System.out.println("Erro: o tamanho deve ser maior que zero.");
+        }
+    }
+	
+	// Método 2
+    // Cada energia vale um minuto de brincar
+	public void brincar(int tempo) {
+	
+	    if (tempo > 0 && energia >= tempo) {
+	
+	        energia = energia - tempo;
+	        System.out.println("O cachorro brincou por " + tempo + " minutos.");
+	
+	    } else {
+	
+	        System.out.println("Erro: energia insuficiente.");
+	    }
+	}
 }
